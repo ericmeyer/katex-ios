63c4c7aa0502150fa907df36a86bd54d381d2868
Eric Meyer <emeyer@8thlight.com>
2020-06-19 13:18:04 -0500

    First commit

---
+++ Katex/Katex.h
@@ -0,0 +1,19 @@
+//
+//  Katex.h
+//  Katex
+//
+//  Created by Eric Meyer on 6/19/20.
+//  Copyright © 2020 Eric Meyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Katex.
+FOUNDATION_EXPORT double KatexVersionNumber;
+
+//! Project version string for Katex.
+FOUNDATION_EXPORT const unsigned char KatexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Katex/PublicHeader.h>
+
+
